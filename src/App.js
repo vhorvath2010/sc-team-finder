6cd3276e67b805f63e44d1c29087a0dc71225be5
Vincent Horvath <vhorvath2010@gmail.com>
2021-10-28 17:03:24 -0400

    Finished ProfileCard draft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Grid } from '@mui/material';
-import { Box } from '@mui/system';
 import './App.css';
 import ProfileCard from './components/ProfileCard';
 
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,25 +1,47 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, List, ListItem, ListItemText, ListSubheader, Typography } from "@mui/material";
+import { fontSize } from '@mui/system';
 
 const theme = createTheme({
     palette: {
-      mode: 'light',
+        mode: 'light',
     },
 });
 
+function CardList(props) {
+    return (
+        <List sx={{ color: "black"}}>
+            <ListSubheader sx={{ color: "black", backgroundColor: "whitesmoke", fontSize: 20}}>{props.title}</ListSubheader>
+            {props.items.map(item => (
+                <ListItem>
+                    <ListItemText primary={item}></ListItemText>
+                </ListItem>
+            ))}
+        </List>
+    );
+}
+
 export default function ProfileCard(props) {
     // Load data (would be done using database calls for profileID in props)
-    var name = "Vince";
+    var name = "Vince",
+        year = "Freshman",
+        skills = ["Java", "Python", "React"],
+        wants = ["Backend", "Data Science"]
 
     return (
         <ThemeProvider theme={theme}>
             <Card sx={{ minWidth: 100, backgroundColor: "whitesmoke" }} variant="outlined">
-            <CardContent>
-                <Typography color="MenuText">
-                    Name: {name}
-                </Typography>
-            </CardContent>
-        </Card>
+                <CardContent>
+                    <Typography sx={{ fontSize: 20}}>
+                        Name: {name}
+                    </Typography>
+                    <Typography sx={{ fontSize: 20}}>
+                        Year: {year}
+                    </Typography>
+                    <CardList items={skills} title="Skills"></CardList>
+                    <CardList items={wants} title="Looking for"></CardList>
+                </CardContent>
+            </Card>
         </ThemeProvider>
     );
 
