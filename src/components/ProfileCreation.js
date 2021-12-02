ee8643550564247d7140802bace388ac95764b4f
Vincent Horvath <vhorvath2010@gmail.com>
2021-12-02 15:01:26 -0500

    Added Firebase and began profile creation

--- src/components/ProfileCreation.js
+++ src/components/ProfileCreation.js
@@ -1,9 +1,40 @@
-import { Dialog, DialogTitle } from "@material-ui/core";
+import { Dialog, DialogTitle, Button, Switch } from "@material-ui/core";
+import TextField from '@mui/material/TextField';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+function makeTextField(id, label, defaultValue) {
+    return (
+        <TextField
+            autoFocus
+            margin="dense"
+            id={id}
+            label={label}
+            type="text"
+            fullWidth
+            variant="standard"
+        />
+    )
+}
 
 export default function ProfileCreation(props, handleClose) {
     return (
-        <Dialog>
-            <DialogTitle>Test</DialogTitle>
-        </Dialog>          
+        <Dialog open={props.open} onClose={handleClose}>
+            <DialogTitle>Edit Profile</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Enter your information below to help us find the perfect teammates for your group!
+                </DialogContentText>
+                {makeTextField('name', 'Name')}
+                {makeTextField('contact', 'Contact Info')}
+                <FormControlLabel control={<Switch defaultChecked={true} />} label="Looking for a team?" />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Subscribe</Button>
+            </DialogActions>
+        </Dialog>
     )
 }
